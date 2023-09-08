e4c3eb756585c44df2a4b6866b6d2d69b28a07d7
tunogya <tunogya@qq.com>
2023-09-08 14:46:48 +0800

    update

--- app/futures/layout.tsx
+++ app/futures/layout.tsx
@@ -19,7 +19,9 @@ export default function Layout(props: { children: ReactNode }) {
     <div className={"h-full w-full flex relative justify-center"}>
       <div className={"max-w-[1920px] w-full h-full flex"}>
         <Navigation active={"/futures"} />
-        <div className={"w-full px-8 mr-10 space-y-2"}>
+        <div
+          className={"w-full px-8 mr-10 space-y-2 overflow-hidden break-all"}
+        >
           <Toolbar />
           {props.children}
         </div>
--- app/op/send/page.tsx
+++ app/op/send/page.tsx
@@ -1,7 +1,37 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
+
 const Send = () => {
-  return <div>Send</div>;
+  const [type, setType] = useState("1");
+  const [botList, setBotList] = useState<string[]>([]);
+  const [bot, setBot] = useState("");
+  const [hasCaption, setHasCaption] = useState(false);
+  const [message, setMessage] = useState("");
+  const [caption, setCaption] = useState("");
+  const [isMarkdown, setIsMarkdown] = useState(false);
+  const [status, setStatus] = useState("idle");
+
+  const fetchBotList = useCallback(async () => {
+    const auth = window.localStorage.getItem("auth");
+    if (auth) {
+      const data = await fetch(
+        `https://cms.nestfi.net/workbench-api/telegram/botlist?type=0`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth}`,
+          },
+        },
+      ).then((res) => res.json());
+      setBotList(data.data);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchBotList();
+  }, [fetchBotList]);
+
+  return <div>{JSON.stringify(botList)}</div>;
 };
 
 export default Send;
