dc0884ee052f9d4cd1cefc0b4f0cb99ea5e32be4
tunogya <tunogya@qq.com>
2023-09-08 14:53:58 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import "cal-sans";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import { TailwindIndicator } from "@/components/TailwindIndicator";
 import * as process from "process";
 import { ReactNode } from "react";
@@ -38,18 +36,6 @@ export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
       <body className={`h-screen w-screen`}>
-        <Script
-          src={"https://www.googletagmanager.com/gtag/js?id=G-HT9Q8GW970"}
-        />
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                
-                gtag('config', 'G-HT9Q8GW970');
-              `}
-        </Script>
         <TailwindIndicator />
         {props.children}
       </body>
