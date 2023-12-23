357d96e228417536cf38fb82e475e78e736926b6
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-23 14:06:35 +0800

    update

--- app/futures/parameters/page.tsx
+++ app/futures/parameters/page.tsx
@@ -3,7 +3,7 @@ import PairCard from "@/app/futures/parameters/PairCard";
 
 const CSR = () => {
   return (
-    <div className={"grid grid-cols-4 gap-2"}>
+    <div className={"grid lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2"}>
       <PairCard pair={"ETH/USDT"} />
       <PairCard pair={"BTC/USDT"} />
       <PairCard pair={"BNB/USDT"} />
@@ -14,11 +14,6 @@ const CSR = () => {
       <PairCard pair={"SOL/USDT"} />
       <PairCard pair={"LTC/USDT"} />
       <PairCard pair={"AVAX/USDT"} />
-      <PairCard pair={"AUD/USD"} />
-      <PairCard pair={"EUR/USD"} />
-      <PairCard pair={"USD/JPY"} />
-      <PairCard pair={"USD/CAD"} />
-      <PairCard pair={"GBP/USD"} />
     </div>
   );
 };
