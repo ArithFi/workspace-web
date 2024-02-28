a02b25f7b25c2db63add0d0819470db56328cd54
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-28 14:09:29 +0800

    stage

--- app/futures/appDownload/page.tsx
+++ app/futures/appDownload/page.tsx
@@ -3,7 +3,7 @@ import IframeResizer from "iframe-resizer-react";
 const Metabase = () => {
   return (
     <IframeResizer
-      src="https://workspace.nestprotocol.org/public/question/41a3a8d2-5ff1-47f0-9b67-22b735c930d2"
+      src="https://workspace.nestprotocol.org/public/dashboard/daba7f01-7d27-48a3-9994-66123d8ae3bf"
       width="100%"
       height="100%"
       scrolling
