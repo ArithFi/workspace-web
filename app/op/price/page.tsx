dea223d9c42eb304159670617a272afdb54b8d64
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-08 11:36:00 +0800

    stage

--- app/op/price/page.tsx
+++ app/op/price/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useSWR from "swr";
+import { useEffect, useState } from "react";
 
 const Price = () => {
   // https://api.binance.com/fapi/v1/ticker/price?symbol=BTCUSDT
@@ -21,21 +22,58 @@ const Price = () => {
     },
   );
 
+  const [btcPrice, setBtcPrice] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket(
+      "wss://fstream.binance.com/ws/btcusdt@markPrice@1s",
+    );
+
+    ws.onmessage = (event) => {
+      const json = JSON.parse(event.data);
+      if (json.p) {
+        setBtcPrice(json.p);
+      }
+    };
+
+    return () => ws.close(); // 组件卸载时关闭连接
+  }, []);
+
   return (
     <div className={"flex flex-col space-y-2"}>
       <div className={"flex space-x-2"}>
-        <div className={"flex flex-col w-fit p-4 border space-y-2 rounded"}>
+        <div
+          className={
+            "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-red-500"
+          }
+        >
           <div className={"text-xs text-gray-600"}>
-            https://fapi.binance.com/fapi/v1/ticker/price
+            REST API: https://fapi.binance.com/fapi/v1/ticker/price
           </div>
           <div className={"text-4xl text-gray-800"}>{binanceFAPI?.price}</div>
         </div>
-        <div className={"flex flex-col w-fit p-4 border space-y-2 rounded"}>
+        <div
+          className={
+            "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-red-300"
+          }
+        >
           <div className={"text-xs text-gray-600"}>
-            https://db.arithfi.com/arithfi_main/oracle/price
+            REST API: https://db.arithfi.com/arithfi_main/oracle/price
           </div>
           <div className={"text-4xl text-gray-800"}>
             {atfAPI?.data.toFixed(2)}
+          </div>
+        </div>
+        <div
+          className={
+            "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-red-300"
+          }
+        >
+          <div className={"text-xs text-gray-600"}>
+            WS API: wss://fstream.binance.com/ws/
+          </div>
+          <div className={"text-4xl text-gray-800"}>
+            {Number(btcPrice).toFixed(2)}
           </div>
         </div>
       </div>
