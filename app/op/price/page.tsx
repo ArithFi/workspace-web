7162d85d7f35ea4ba6ae137111d23d61598c4d6a
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-08 13:22:18 +0800

    stage

--- app/op/price/page.tsx
+++ app/op/price/page.tsx
@@ -7,7 +7,7 @@ const Price = () => {
   const [symbol, setSymbol] = useState("DOGE");
   // https://api.binance.com/fapi/v1/ticker/price?symbol=BTCUSDT
   const { data: binanceFAPI, mutate: mutateBinance } = useSWR(
-    `https://fapi.binance.com/fapi/v1/ticker/price?symbol=${symbol}USDT`,
+    `https://fapi.binance.com/fapi/v2/ticker/price?symbol=${symbol}USDT`,
     (url: string) => fetch(url).then((res) => res.json()),
     {
       refreshInterval: 200,
@@ -27,13 +27,13 @@ const Price = () => {
 
   useEffect(() => {
     const ws = new WebSocket(
-      `wss://fstream.binance.com/ws/${symbol.toLowerCase()}usdt@aggTrade`,
+      `wss://fstream.binance.com/ws/${symbol.toLowerCase()}usdt@ticker`,
     );
 
     ws.onmessage = (event) => {
       const json = JSON.parse(event.data);
-      if (json.p) {
-        setWsPrice(json.p);
+      if (json.c) {
+        setWsPrice(json.c);
       }
     };
 
@@ -63,11 +63,21 @@ const Price = () => {
       <div className={"flex space-x-2"}>
         <div
           className={
+            "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-blue-500"
+          }
+        >
+          <div className={"text-xs text-gray-600"}>
+            WS API: wss://fstream.binance.com/ws/
+          </div>
+          <div className={"text-4xl text-gray-800"}>{Number(wsPrice)}</div>
+        </div>
+        <div
+          className={
             "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-red-500"
           }
         >
           <div className={"text-xs text-gray-600"}>
-            REST API: https://fapi.binance.com/fapi/v1/ticker/price
+            REST API: https://fapi.binance.com/fapi/v2/ticker/price
           </div>
           <div className={"text-4xl text-gray-800"}>{binanceFAPI?.price}</div>
         </div>
@@ -80,16 +90,6 @@ const Price = () => {
             REST API: https://db.arithfi.com/arithfi_main/oracle/price
           </div>
           <div className={"text-4xl text-gray-800"}>{atfAPI?.data}</div>
-        </div>
-        <div
-          className={
-            "flex flex-col w-fit p-4 border-2 space-y-2 rounded border-blue-500"
-          }
-        >
-          <div className={"text-xs text-gray-600"}>
-            WS API: wss://fstream.binance.com/ws/
-          </div>
-          <div className={"text-4xl text-gray-800"}>{Number(wsPrice)}</div>
         </div>
       </div>
     </div>
