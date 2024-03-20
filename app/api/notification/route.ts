19154b889a7d5b9cdfe61385983190c971a12c8e
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-20 19:35:01 +0800

    stage

---
+++ app/api/notification/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest } from "next/server";
+import { Expo, ExpoPushMessage } from "expo-server-sdk";
+
+let expo = new Expo({
+  useFcmV1: true, // this can be set to true in order to use the FCM v1 API
+});
+
+const POST = async (req: NextRequest) => {
+  const { pushTokens, title, body, data } = await req.json();
+
+  let messages: ExpoPushMessage[] = [];
+  for (let pushToken of pushTokens) {
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+    messages.push({
+      to: pushToken,
+      title: title,
+      body: body,
+      data: data || undefined,
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    // @ts-ignore
+    if (ticket?.id) {
+      // @ts-ignore
+      receiptIds.push(ticket?.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+
+  let receiptsMap = {};
+  for (let chunk of receiptIdChunks) {
+    try {
+      let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+      receiptsMap = {
+        ...receiptsMap,
+        ...receipts,
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return new Response(JSON.stringify(receiptsMap), { status: 200 });
+};
+
+export { POST };
---
+++ app/op/notification/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Page = () => {
+  const [token, setToken] = useState("");
+  const [form, setForm] = useState({
+    pushTokens: [],
+    title: "",
+    body: "",
+    data: "",
+  });
+  const [status, setStatus] = useState("idle");
+  const [result, setResult] = useState([]);
+
+  const fetchList = async (start: number, count: number) => {
+    const res = await fetch(
+      `https://db.nestfi.net/arithfi/maintains/listNotification?start=${start}&count=${count}`,
+    ).then((res) => res.json());
+
+    if (res?.data?.length > 0) {
+      const availableTokens = res.data
+        .filter((item: any) => item.marketing)
+        .map((item: any) => item.pushToken);
+      setForm({
+        ...form,
+        pushTokens: availableTokens,
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchList(0, 100);
+  }, []);
+
+  const send = async () => {
+    setStatus("loading");
+    try {
+      const res = await fetch("/api/notification", {
+        method: "POST",
+        headers: {
+          Authorization: `${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      }).then((res) => res.json());
+      console.log(res);
+      setStatus("idle");
+    } catch (e) {
+      console.log(e);
+      setStatus("error");
+    }
+  };
+
+  return (
+    <div className={"w-full h-full flex flex-col pb-2"}>
+      <div className={"w-full h-60 border-b p-4"}>
+        <div className={"text-sm font-medium"}>待发送列表:</div>
+        <div className={"flex flex-row"}>
+          {form.pushTokens.map((item: string, index) => (
+            <div key={index}>{item}</div>
+          ))}
+        </div>
+      </div>
+      <div className={"flex-1 flex gap-2"}>
+        <div className={"w-[40%] border-r"}>
+          <div className={"w-full flex flex-col gap-2 p-4"}>
+            <label className={"text-xs font-bold"}>签名</label>
+            <input
+              value={token}
+              placeholder={"Token"}
+              className={"border p-2 text-sm"}
+              onChange={(e) => setToken(e.target.value)}
+            />
+          </div>
+          <div className={"w-full flex flex-col gap-2 p-4"}>
+            <label className={"text-xs font-bold"}>消息标题</label>
+            <input
+              value={token}
+              placeholder={"Token"}
+              className={"border p-2 text-sm"}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  title: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className={"w-full flex flex-col gap-2 p-4"}>
+            <label className={"text-xs font-bold"}>消息内容</label>
+            <input
+              value={token}
+              placeholder={"Token"}
+              className={"border p-2 text-sm"}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  body: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className={"w-full flex flex-col gap-2 p-4"}>
+            <label className={"text-xs font-bold"}>消息跳转链接</label>
+            <input
+              value={token}
+              placeholder={"Token"}
+              className={"border p-2 text-sm"}
+              onChange={(e) =>
+                setForm({
+                  ...form,
+                  data: `/webview?url=${e.target.value}`,
+                })
+              }
+            />
+          </div>
+          <div className={"flex justify-end p-4"}>
+            <button
+              className={`text-sm bg-[#0D6EFD] text-white p-2 rounded-[4px] disabled:opacity-50`}
+              onClick={send}
+              disabled={status !== "idle"}
+            >
+              发送消息
+            </button>
+          </div>
+        </div>
+        <div className={"flex-1"}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
